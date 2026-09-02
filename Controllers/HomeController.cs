a1e699bb9acc7ce74d41bcc6b39b41aa26589e30
Emirhan Basaran <emirhanbasaran2002@gmail.com>
2026-09-02 11:09:12 +0300

    Ana sayfa istatistikleri duzeltildi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,68 +5,74 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace KutuphaneOtomasyonu.Controllers
+namespace KutuphaneOtomasyonu.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class HomeController : Controller
 {
-    [Authorize(Roles = "Admin")]
-    public class HomeController : Controller
+    private readonly KutuphaneContext _context;
+
+    public HomeController(KutuphaneContext context)
     {
-        private readonly KutuphaneContext _context;
+        _context = context;
+    }
 
-        public HomeController(KutuphaneContext context)
+    public async Task<IActionResult> Index()
+    {
+        DateTime bugun = DateTime.Today;
+
+        // Sistemdeki toplam kitap kaydı
+        ViewBag.ToplamKitap =
+            await _context.Kitaplars.CountAsync();
+
+        // Yalnızca aktif üye sayısı
+        ViewBag.ToplamUye =
+            await _context.Uyelers.CountAsync(u =>
+                u.Rol == "Uye" &&
+                u.AktifMi);
+
+        // Teslim edilmemiş fakat henüz gecikmemiş kitaplar
+        ViewBag.OdunctekiKitap =
+            await _context.OduncIslemleris.CountAsync(o =>
+                !o.TeslimEdildiMi &&
+                o.SonTeslimTarihi.Date >= bugun);
+
+        // Teslim edilmemiş ve teslim tarihi geçmiş kitaplar
+        ViewBag.GecikenKitap =
+            await _context.OduncIslemleris.CountAsync(o =>
+                !o.TeslimEdildiMi &&
+                o.SonTeslimTarihi.Date < bugun);
+
+        // Son beş ödünç işlemi
+        var sonIslemler =
+            await _context.OduncIslemleris
+                .Include(o => o.Kitap)
+                .Include(o => o.Uye)
+                .OrderByDescending(o =>
+                    o.VerilisTarihi)
+                .Take(5)
+                .ToListAsync();
+
+        return View(sonIslemler);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [AllowAnonymous]
+    [ResponseCache(
+        Duration = 0,
+        Location = ResponseCacheLocation.None,
+        NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel
         {
-            _context = context;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            ViewBag.ToplamKitap =
-                await _context.Kitaplars.CountAsync();
-
-            ViewBag.ToplamUye =
-                await _context.Uyelers
-                    .CountAsync(u => u.Rol == "Uye");
-
-            ViewBag.OdunctekiKitap =
-                await _context.OduncIslemleris
-                    .CountAsync(o => !o.TeslimEdildiMi);
-
-            ViewBag.GecikenKitap =
-                await _context.OduncIslemleris
-                    .CountAsync(o =>
-                        !o.TeslimEdildiMi &&
-                        o.SonTeslimTarihi.Date <
-                        DateTime.Today);
-
-            var sonIslemler =
-                await _context.OduncIslemleris
-                    .Include(o => o.Kitap)
-                    .Include(o => o.Uye)
-                    .OrderByDescending(o =>
-                        o.VerilisTarihi)
-                    .Take(5)
-                    .ToListAsync();
-
-            return View(sonIslemler);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [AllowAnonymous]
-        [ResponseCache(
-            Duration = 0,
-            Location = ResponseCacheLocation.None,
-            NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel
-            {
-                RequestId =
-                    Activity.Current?.Id ??
-                    HttpContext.TraceIdentifier
-            });
-        }
+            RequestId =
+                Activity.Current?.Id ??
+                HttpContext.TraceIdentifier
+        });
     }
 }
